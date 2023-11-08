f79b31b6c05eebc99e8f7600747db5b5080b50f1
Regis JOUAN <regis.jouan@lapiscine.pro>
2023-11-08 14:01:19 +0100

    react objects in an array

--- src/Products.js
+++ src/Products.js
@@ -1,17 +1,42 @@
 function Products () {
 
+  
     const productsFromDb = [
-        "Produit 1", "Produit 2", "Produit 3"
+        {
+        title: "Chaussures vertes",
+        price: 300,
+        isPubliched: true
+    },
+    {
+        title: "Chaussettes rouges",
+        price: 100,
+        isPubliched: true
+    },
+    {
+        title: "Lacets bleus",
+        price: 20,
+        isPubliched: true
+    },
+    {
+        title: "Oeillets jaunes",
+        price:80,
+        isPubliched: true
+    },
     ];
 
     return (
         <main class="mainstyle">
+
+
             {productsFromDb.map((product) => {
-            return (
-                <article>
-                    <h2>{product}</h2>
-                </article>
-            );
+                return (
+                    <article>
+                        <>
+                            <h2>{product.title}</h2>
+                            <p>{product.price} €</p>
+                       </>
+                    </article>
+                );
             })}
         </main>       
     );
